a39df857b9a51b222636c44cb619407d46320aa4
Nikhil Kumar S <nikhilkumars12343@gmail.com>
2025-06-01 16:08:20 +0530

    set up radix ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +27,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         
-      >
-        <NavBar/>
-        <main>{children}</main>
-        
+        <Theme>
+            <NavBar/>
+            <main>{children}</main>
+        </Theme>
+  
       </body>
     </html>
   );
